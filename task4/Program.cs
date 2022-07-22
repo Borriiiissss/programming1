7b86d76aa2a923b591d5857cd92b38311e80ddec
Borriiiissss <chega123@mail.ru>
2022-07-22 19:09:25 +0200

    задания после семинара 1

---
+++ task4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int num1 = 0, num2 = 0, num3 = 0;
+
+Console.WriteLine("введите число 1 ");
+string userenter1 = Console.ReadLine();
+num1 = int.Parse(userenter1);
+
+Console.WriteLine("введите число 2 ");
+string userenter2 = Console.ReadLine();
+num2 = int.Parse(userenter2);
+
+Console.WriteLine("введите число 3 ");
+string userenter3 = Console.ReadLine();
+num3 = int.Parse(userenter3);
+
+if (num1 > num2 && num1 > num3)
+{
+    System.Console.WriteLine($"{num1}, {num2}, {num3} -> max = {num1} ");
+}
+else if  (num2 > num1 && num2 > num3)
+{
+
+    System.Console.WriteLine($"{num1}, {num2}, {num3} -> max = {num2} ");
+}
+else
+    {System.Console.WriteLine($"{num1}, {num2}, {num3} -> max = {num3} ");}
---
+++ task6/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+int num1 = 0, check;
+
+Console.WriteLine("введите число ");
+string userenter1 = Console.ReadLine();
+num1 = int.Parse(userenter1);
+
+check = num1 % 2;
+System.Console.WriteLine(check);
+
+if (check == 0)
+{
+    System.Console.WriteLine($"{num1} -> да ");
+}
+else 
+{
+    System.Console.WriteLine($"{num1} -> нет ");
+}
