c7cef8d3409c42735d1059a483f9af1efa07ed3f
Borriiiissss <chega123@mail.ru>
2022-07-25 10:57:10 +0200

    задачи семинара2

---
+++ task13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+int inp = int.Parse(Console.ReadLine());
+
+void Show (int anynum)
+{
+    
+    string output = inp.ToString();
+    if (output.Length > 2) 
+        Console.WriteLine($"{inp} -> {output [2]}");
+    else 
+        Console.WriteLine ("третья цифра отсутствует");       
+}
+
+Show (inp);
---
+++ task15/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+int InputNumber()
+{
+    Console.Write("введите цифру ");
+    int num = int.Parse(Console.ReadLine());
+    Console.Write(num);
+    return num;
+}
+string CheckDay(int num)
+{
+    string checkresult;
+    if (num > 5)
+        checkresult = "да";
+    else 
+        checkresult = "нет";
+    return checkresult;
+}
+Console.Write($" -> {CheckDay(InputNumber ())}");
