a3654b3902ef100e2026043787cfe47c76637216
Borriiiissss <chega123@mail.ru>
2022-07-25 10:05:46 +0200

    задача 10

---
+++ task10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+void Show ()
+{
+    int inp = int.Parse(Console.ReadLine());
+    string output = inp.ToString();
+    Console.WriteLine($"{inp} -> {output [1]}");
+}
+
+Show ();
