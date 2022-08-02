9224cca77db4a74c86ddbde3bb4667b66495c9e2
Borriiiissss <chega123@mail.ru>
2022-08-02 12:00:32 +0200

    задача4143

---
+++ tmp/Program.cs
@@ -0,0 +1,13 @@
+﻿// наименьший делитель
+int  size = 0;
+
+int IsItNumber()
+{
+if (int.TryParse(System.Console.ReadLine()!, out size))
+System.Console.WriteLine("yes");
+else
+{System.Console.WriteLine("no");
+IsItNumber();}
+return size;
+}
+System.Console.WriteLine(IsItNumber());
